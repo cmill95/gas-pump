eb01bead8ec09f2023504d0d13c811472a3e2df1
mvanlyssel <mvanlyssel@unm.edu>
2025-09-05 12:54:20 -0600

    HoseGUI

--- src/ServerMain.java
+++
@@ -1,7 +0,0 @@
-/**
- * This class creates the server which the Main board will communicate with. It
- * is responsible for determining whether a customer can or cannot purchase gas.
- */
-
-public class ServerMain {
-}
--- src/SocketHandler.java
+++
@@ -1,7 +0,0 @@
-/**
- * This class creates the sockets which are used by all the elements of the Gas Pump
- * to communicate with each other. It employs an API to normalize this communication.
- */
-
-public class SocketHandler {
-}
